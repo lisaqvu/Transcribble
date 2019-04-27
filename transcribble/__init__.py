85ee55846d71e6dc265fa8593664f5a9b0953412
Lisa V <lisaqv@bu.edu>
2019-04-27 14:09:57 -0400

    refactoring

---
+++ transcribble/__init__.py
@@ -0,0 +1,97 @@
+import os
+
+from flask_environments import Environments
+from flask import request, Flask, render_template
+from google.cloud import storage
+import logging
+import yaml, json
+
+with open('././app.yaml') as f:
+    envfile = yaml.safe_load(f)
+
+def create_app(test_config=None):
+    from . import edit, export, transcribe, translate, db
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        #DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    # hacky fix
+    app.config.update(envfile['env_variables'])
+    os.environ.update(envfile['env_variables'])
+
+    CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+
+    @app.route('/')
+    def index():
+        # return render_template("base.html")
+        return """
+    <form method="POST" action="/upload" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit">
+    </form>
+    """
+    # HELPER TO GET NEXT ID FROM SQL
+
+    @app.route('/upload', methods=['POST'])
+    def upload():
+        """Process the uploaded file and upload it to Google Cloud Storage."""
+        uploaded_file = request.files.get('file')
+
+        if not uploaded_file:
+            return 'No file uploaded.', 400
+
+        # Create a Cloud Storage client.
+        gcs = storage.Client()
+
+        # Get the bucket that the file will be uploaded to.
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(uploaded_file.filename)
+
+        blob.upload_from_string(
+            uploaded_file.read(),
+            content_type=uploaded_file.content_type
+        )
+
+        # The public URL can be used to directly access the uploaded file via HTTP.
+        return blob.public_url
+
+
+    @app.errorhandler(500)
+    def server_error(e):
+        logging.exception('An error occurred during a request.')
+        return """
+        An internal error occurred: <pre>{}</pre>
+        See logs for full stacktrace.
+        """.format(e), 500
+
+
+    if __name__ == '__main__':
+        # This is used when running locally. Gunicorn is used to run the
+        # application on Google App Engine. See entrypoint in app.yaml.
+        app.run(host='127.0.0.1', port=8080, debug=True)
+
+    return app
+    
---
+++ transcribble/transcribe/Parse.py
@@ -0,0 +1,82 @@
+from Transcription import *
+
+import soundfile
+import librosa
+import librosa.display
+import librosa.output
+
+from moviepy.editor import *
+
+import os.path as path
+import os
+
+# from pydub import AudioSegment
+
+
+def parseSrt(file):
+
+    f = open(file, 'r')
+
+    trans = Transcription()
+    timestamp = None
+    line_num = 0
+
+    for line in f:
+        line_num += 1
+
+        if line == '\n':
+            line_num = 0
+        elif line_num == 2:
+            line = line[:-1]
+            start_time = line[:12]
+            end_time = line[-12:]
+            timestamp = Timestamp(start_time, end_time)
+        elif line_num == 3:
+            line = line[:-1]
+            trans.appendSentence(Sentence(timestamp, line))
+
+    return trans
+
+def parseAudio(file, start_time = -1, end_time = -1):
+    if not path.exists(r'./temp_files'):
+        os.mkdir(r'./temp_files')
+    temp_file = path.join(r'./temp_files', path.splitext(path.basename(file))[0] + '.wav')
+
+    # audio files must be mono or stereo (5.1 is not supported)
+    # .flac is not supported
+    #sound = AudioSegment.from_file(file, path.splitext(file)[1][1:])
+    #sound = sound.set_sample_width(2)
+    #sound.export(temp_file, format = 'wav')
+
+    y, sr = librosa.load(file, mono = True, sr = None)
+
+    if start_time == -1:
+        start_time = 0
+    else:
+        if not 0<=start_time<=len(y)/sr:
+            raise TypeError('Illegal start_time')
+        start_time = int(start_time*sr)
+    if end_time == -1:
+        end_time = len(y) + 1
+    else:
+        if not 0<=end_time<=len(y)/sr:
+            raise TypeError('Illegal end_time')
+        end_time = int(end_time*sr) + 1
+
+    y = y[start_time:end_time]
+
+    librosa.output.write_wav(temp_file, y, sr)
+
+    data, samplerate = soundfile.read(temp_file)
+    soundfile.write(temp_file, data, samplerate, subtype='PCM_16')
+
+    return librosa.display.waveplot(y, sr = sr)
+
+def parseVideo(file, start_time = -1, end_time = -1):
+    if not path.exists(r'./temp_files'):
+        os.mkdir(r'./temp_files')
+    temp_file = path.join(r'./temp_files', path.splitext(path.basename(file))[0] + '.wav')
+
+    audioclip = AudioFileClip(file)
+    audioclip.write_audiofile(temp_file)
+    parseAudio(temp_file, start_time = -1, end_time = -1)
